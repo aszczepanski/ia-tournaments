3d7f8833ab667d15cbb99e4324c5fff63a337606
Adam Szczepański <aszczepanski92@gmail.com>
2014-06-26 16:21:51 +0200

    removed 'help' static page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/help', to: 'static_pages#help', via: 'get'
 
   resources :tournaments, only: [:new, :create, :edit, :update, :show, :destroy]
   get 'tournaments/:id/join' => 'tournaments#new_join', as: :new_join_tournament
